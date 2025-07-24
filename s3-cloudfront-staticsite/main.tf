ccfc8ab76032f8f84a95526826bff297ad1a693c
DevOpsStudent-18 <karthikagitmail1989@gmail.com>
2025-07-24 19:09:42 +0530

    Update main.tf

--- s3-cloudfront-staticsite/main.tf
+++ s3-cloudfront-staticsite/main.tf
@@ -5,8 +5,31 @@ resource "aws_s3_bucket" "tfbucket"{
 resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.tfbucket.id
   for_each = fileset("aws-s3-static-website-sample/Website/", "**/*.*")
+
     key    = each.value
-    source = "aws-s3-static-website-sample/Website/${each.value}"  
+    source = "aws-s3-static-website-sample/Website/${each.value}"
+
+  
+}
+resource "aws_s3_bucket_public_access_block" "landing_page_public_access" {
+  bucket = aws_s3_bucket.tfbucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site" {
+  bucket = aws_s3_bucket.tfbucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
@@ -39,6 +62,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     default_ttl            = 3600
     max_ttl                = 86400
   }
+
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
@@ -46,7 +71,28 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
   }
 
+
+
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+}
+
+resource "aws_s3_bucket_policy" "landing_page_bucket_policy" {
+  bucket = aws_s3_bucket.tfbucket.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowGetObj",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.tfbucket.id}/*"
+    }
+  ]
+}
+POLICY
 }
